ef12f2da19d2016618d785fc64bd8e9285011a37
Nicholas1Front <nicholasdeoliveiraeugenio@gmail.com>
2024-07-11 12:16:06 -0300

    added know-facam-links and some animations 11/07 12:16

--- app/index/scripts/indexScript.js
+++ app/index/scripts/indexScript.js
@@ -1,5 +1,44 @@
-// testing
+// elements
 
-console.log("testing javascript...")
+const linksControl = document.querySelectorAll(".links-control");
+const linksControl_Symbol = document.querySelectorAll(".links-control i");
+const linksControl_Link = document.querySelectorAll(".links a");
 
-// working beautiful !
+// functions
+
+function hoverInLinkControl(){
+    linksControl_Symbol.forEach((symbol) => {
+        symbol.style.fontWeight = "900";
+        symbol.style.color = "#00489A";
+        symbol.style.transition = "0.2s";
+    })
+
+    linksControl_Link.forEach((link) => {
+        link.style.fontWeight = "900";
+        link.style.color = "#00489A";
+        link.style.transition = "0.2s";
+    })
+}
+
+function hoverOutLinkControl(){
+    linksControl_Symbol.forEach((symbol) => {
+        symbol.style.fontWeight = "bold";
+        symbol.style.color = "black";
+        symbol.style.transition = "0.2s";
+    })
+
+    linksControl_Link.forEach((link) => {
+        link.style.fontWeight = "normal";
+        link.style.color = "black";
+        link.style.transition = "0.2s";
+    })
+}
+
+// event listeners
+
+linksControl.forEach((link) => {
+    link.addEventListener("mouseenter", function(event){
+        event.preventDefault();
+        hoverInLinkControl();
+    });
+})
