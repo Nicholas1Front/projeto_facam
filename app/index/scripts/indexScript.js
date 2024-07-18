0038c96c10f845ccd7b0a8541c36ab0082d23d06
Nicholas1Front <nicholasdeoliveiraeugenio@gmail.com>
2024-07-18 18:38:00 -0300

    added back to the top button

--- app/index/scripts/indexScript.js
+++ app/index/scripts/indexScript.js
@@ -1,7 +1,12 @@
 //elements
+const getKnow_section = document.querySelector(".get-know-section")
+const graduation_section = document.querySelector(".graduation-section");
+
 const linksControl = document.querySelectorAll(".links-control");
 const linksControl_symbol = document.querySelectorAll(".links-control i");
 const linksControl_link = document.querySelectorAll(".links-control a");
+
+const registration_section = document.querySelector(".registration-section");
 
 const slides = document.querySelectorAll(".slide");
 const slideNextBtn = document.querySelector(".slide-next-control");
@@ -13,6 +18,9 @@ const coursesControl = document.querySelectorAll(".courses-items-control");
 const coursesImgs = document.querySelectorAll(".course-img");
 const coursesImgsHover = document.querySelectorAll(".course-img-hover");
 const coursesLinks = document.querySelectorAll(".courses-items-control a");
+
+const backTopControl = document.querySelector(".back-to-top-control");
+const backTopBtn = document.querySelector(".back-to-top-btn");
 
 // testing
 
@@ -93,10 +101,17 @@ function nextSlideProcess(){
 
 }
 
+let graduation_section_topScrollPosition = 0;
+let registration_section_topScrollPosition = 0;
+
 function autoplaySlides(scrollValue){
-    if(scrollValue >= 610){
+
+    graduation_section_topScrollPosition = getElementTopPosition(graduation_section);
+    registration_section_topScrollPosition = getElementTopPosition(registration_section);
+
+    if(scrollValue >= registration_section_topScrollPosition){
         nextSlideProcess();
-    }else if(scrollValue > 1510 || scrollValue < 550){
+    }else if(scrollValue > graduation_section_topScrollPosition || scrollValue < registration_section_topScrollPosition){
         return;
     }
 
@@ -172,6 +187,18 @@ function hoverOutLinksControl(param){
     linksControl_link[param].style.transition = "0.2s";
 }
 
+/*
+Esta função pega a altura em pixels de um elemento a partir do inicio do página,
+utilizando element como argumento. 
+*/
+
+function getElementTopPosition(element) {
+    const rect = element.getBoundingClientRect();
+    const scrollTop = window.scrollY || window.pageYOffset;
+    
+    return rect.top + scrollTop;
+}
+
 function hoverInCoursesControl(param){
     coursesImgsHover[param].style.display = "block";
     coursesImgsHover[param].style.transition = "0.2s";
@@ -194,6 +221,26 @@ function hoverOutCoursesControl(param){
     coursesLinks[param].style.transition = "0.2s";
 }
 
+function scrollToTop(){
+    window.scrollTo({
+        top : document.body.offsetTop,
+        behavior : "smooth",
+    })
+}
+
+function showBackTopBtn(){
+    let scrollValue = window.scrollY || document.documentElement.scrollTop;
+
+    if(scrollValue > 300){
+        backTopControl.style.marginTop = "44%";
+        backTopControl.style.marginLeft = "93%";
+        backTopControl.style.transition = "0.3s";
+    }else if (scrollValue < 300){
+        backTopControl.style.marginTop = "100%";
+        backTopControl.style.marginLeft = "110%";
+        backTopControl.style.transition = "0.3s";
+    }
+}
 
 // booting 
 
@@ -230,5 +277,13 @@ document.addEventListener('DOMContentLoaded',function(event){
         })
     };
     
+});
+
+window.addEventListener("scroll",function(){
+    showBackTopBtn();
+})
+
+backTopBtn.addEventListener("click",function(){
+    scrollToTop();
 })
 
